a28b9a9dac5f5c4d9c53355ebd6e5945bcc7c2e0
Apple <apple@Apples-MacBook-Air.local>
2022-07-26 23:58:43 -0400

    update

--- app.js
+++ app.js
@@ -17,10 +17,10 @@ const serviceAccount = {
   "private_key": process.env.PRIV_KEY.replace(/\\n/g, '\n'),
   "client_email": process.env.CL_EMAIL,
   "client_id": process.env.CL_ID,
-  "auth_uri": process.env.AUTH_URL,
-  "token_uri": process.env.TOKEN_URL,
-  "auth_provider_x509_cert_url": process.env.AUTH,
-  "client_x509_cert_url": process.env.CERT_URL
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-oxfsb%40furever-7cf38.iam.gserviceaccount.com"
 };
 
 
